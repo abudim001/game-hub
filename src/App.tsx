db397d415d15596be8d55d0d05bde124f2f34185
Adem Hussen <abudim001@gmail.com>
2024-12-17 23:05:24 -0800

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import { Box } from "@chakra-ui/react";
-import { Button } from "@/components/ui/button";
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
-  return <Button>Button</Button>;
+  const isLargScreen = useBreakpointValue({ base: false, lg: true });
+  return (
+    <Grid
+      gridTemplateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+      <Show when={isLargScreen}>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="cyan">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
